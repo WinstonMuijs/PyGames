e0c1cb1ef867f5ee08c82fc1f1b1a046d3073ddc
Winston Muijs <56136891+WinstonMuijs@users.noreply.github.com>
2023-01-05 14:53:57 +0100

    Uploaded the main files for a shooter game
    Main code for a simple shooter game in Python, with graphics, sounds and movement.

---
+++ main.py
@@ -0,0 +1,51 @@
+import pygame
+import sys
+
+pygame.init()
+
+WIDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+display_surface = pygame.display.set_mode((WIDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Galaxy Shooter")
+clock = pygame.time.Clock()  # limit max frame rate
+# create a surface
+# test = pygame.Surface((400, 100))
+# We need to attach the surface to the display_surface -blit methode
+
+# importing images&text
+# converting for optimizing
+spaceship = pygame.image.load('graphics/ship.png').convert_alpha()
+spaceship_rec = spaceship.get_rect(center=(WIDOW_WIDTH/2, WINDOW_HEIGHT/2))  # creating rectangles for placing and movement
+background = pygame.image.load('graphics/background.png').convert()
+
+# import text
+font = pygame.font.Font('graphics/subatomic.ttf', 50)
+text = font.render("Galaxy", True, "seagreen1")
+text_rec = text.get_rect(center=(WIDOW_WIDTH/2, WINDOW_HEIGHT-25))
+
+# Keeps the game running in a loop
+while True:
+    # 1. input of the player(keyboard, mouse, press button,..)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()   # quits the game
+            sys.exit()
+    # frame rate
+    clock.tick(120)
+
+    # 2. updates,
+    # layers of the game, orders matter
+    display_surface.fill((0, 0, 0))  # display_surface
+    display_surface.blit(background, (0, 0))  # background
+
+    if spaceship_rec.top > 0:  # if spaceship at top of window, stop movement
+        spaceship_rec.y -= 4  # movement per cycle or iteration of the while loop
+
+    display_surface.blit(spaceship, spaceship_rec ) # positioning surface and control movement
+    display_surface.blit(text, text_rec)
+
+    # test.fill("navy")             # surface
+    # # placing
+    # display_surface.blit(test, (880, 360))  # positioning the surface on the display
+
+    # 3. Show the frame/surface to the player
+    pygame.display.update()
